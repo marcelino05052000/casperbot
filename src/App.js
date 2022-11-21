6ca01cf698a9776c9ba6068c584d31e8fe50fa0a
Marcelino Barros <marcelino05052000@gmail.com>
2022-11-21 01:39:17 -0300

    Adicionando dados no banco

--- src/App.js
+++ src/App.js
@@ -2,11 +2,21 @@ import './App.css';
 import Home from './pages/home';
 import { useEffect, useState } from 'react';
 import { db } from './firebase'
-import { collection, getDocs } from "firebase/firestore";
+import { collection, getDocs, addDoc } from "firebase/firestore";
 
 function App() {
+  const [title, set_title] = useState("");
+  const [subtitle, set_subtitle] = useState("");
+  const [b_title, set_b_title] = useState("");
+  const [b_url, set_b_url] = useState("");
+  const [i_url, set_i_url] = useState("");
+
   const [entretenimento, setEntretenimento] = useState([]);
   const entretenimentoCollectionRef = collection(db, "faq.entretenimento");
+
+  const criarEntretenimento = async () => {
+    await addDoc(entretenimentoCollectionRef, {title: title, subtitle: subtitle, b_title: b_title, b_url: b_url, i_url: i_url})
+  }
 
   useEffect(() => {
     const getEntretenimento = async () => {
@@ -18,6 +28,22 @@ function App() {
 
   return (
     <div className="App">
+      <input placeholder='Titulo: ' onChange={(event => {
+        set_title(event.target.value);
+      })} />
+      <input placeholder='Subtitulo: ' onChange={(event => {
+        set_subtitle(event.target.value);
+      })} />
+      <input placeholder='Titulo do botão: ' onChange={(event => {
+        set_b_title(event.target.value);
+      })} />
+      <input placeholder='Url do botão: ' onChange={(event => {
+        set_b_url(event.target.value);
+      })} />
+      <input placeholder='Url da imagem: ' onChange={(event => {
+        set_i_url(event.target.value);
+      })} />
+      <button onClick={criarEntretenimento}>Adicionar notícia</button>
       {entretenimento.map((entr) => {
         return (
           <div>
