8ea9232c0d76e76ec96a2a0e17c089e58c7b1988
Marcelino Barros <marcelino05052000@gmail.com>
2022-11-22 00:02:42 -0300

    Adicionada ligação com Esportes

---
+++ src/services/esportes.services.js
@@ -0,0 +1,39 @@
+import { db } from "../firebase";
+
+import {
+    collection,
+    getDocs,
+    getDoc,
+    addDoc,
+    updateDoc,
+    deleteDoc,
+    doc,
+} from "firebase/firestore";
+
+const esportesCollectionRef = collection(db, "faq.esportes");
+class EsportesServices {
+    addEsportes = (newEspo) => {
+        return addDoc(esportesCollectionRef, newEspo);
+    };
+
+    updateEsportes = (id, updatedEsportes) => {
+        const entrDoc = doc(db, "faq.esportes", id);
+        return updateDoc(entrDoc, updatedEsportes);
+    };
+
+    deteleEsportes = (id) => {
+        const entrDoc = doc(db, "faq.esportes", id);
+        return deleteDoc(entrDoc);
+    };
+
+    getAllEsportes = () => {
+        return getDocs(esportesCollectionRef);
+    };
+
+    getEsportes = (id) => {
+        const entrDoc = doc(db, "faq.esportes", id);
+        return getDoc(entrDoc);
+    };
+}
+
+export default new EsportesServices();
